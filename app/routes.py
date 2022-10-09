453e73a64a0b7f449c3f405a2b66236038271798
Per John <perjohn66@icloud.com>
2022-10-09 16:24:44 +0200

    Set status code for illegal move

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
+from http import HTTPStatus
 from uuid import uuid4
 
-from fastapi import APIRouter, Response, Depends
+from fastapi import APIRouter, Depends, Response, status
 
 from app.game.match import Match
 from app.game.move import Move
+from app.game.move_result import MoveResult
 from app.game.player import Player
 from app.schema import NewMatchSchema
 from app.session import session_backend, cookie, SessionData, verifier
@@ -20,8 +22,12 @@ def new_match(response: Response):
     return {'board': match.get_board_as_array()}
 
 
-@router.get("/move/{player}/{column}", response_model=NewMatchSchema, dependencies=[Depends(cookie)])
-def move(player: int, column: int, session_data: SessionData = Depends(verifier)):
+@router.get("/move/{player}/{column}", response_model=NewMatchSchema, dependencies=[Depends(cookie)],
+            status_code=status.HTTP_200_OK)
+def move(player: int, column: int, response: Response, session_data: SessionData = Depends(verifier)):
     match = Match.from_array(session_data.match)
-    match.move(Move(column=column, player=Player.RED if player == 1 else Player.YELLOW))
+    move_result = match.move(Move(column=column, player=Player.RED if player == 1 else Player.YELLOW))
+    if move_result == MoveResult.ILLEGAL_MOVE:
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return {'board': match.get_board_as_array()}
     return {'board': match.get_board_as_array()}
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from fastapi.testclient import TestClient
 
@@ -6,7 +8,7 @@ from app.app import app
 
 def test_new_game(test_client):
     response = test_client.get('api/v1/new-match')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert 'board' in response_json
     assert len(response_json['board']) == 6
@@ -15,10 +17,10 @@ def test_new_game(test_client):
 
 def test_move(test_client):
     response = test_client.get('api/v1/new-match')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     response = test_client.get('api/v1/move/1/3')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert 'board' in response_json
     board = response_json['board']
@@ -29,13 +31,13 @@ def test_move(test_client):
 
 def test_two_moves(test_client):
     response = test_client.get('api/v1/new-match')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     response = test_client.get('api/v1/move/1/3')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     response = test_client.get('api/v1/move/-1/3')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     response_json = response.json()
     assert 'board' in response_json
@@ -45,10 +47,10 @@ def test_two_moves(test_client):
 
 def test_illegal_move(test_client):
     response = test_client.get('api/v1/new-match')
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
     response = test_client.get('api/v1/move/1/8')
-    assert response.status_code == 422
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 @pytest.fixture
